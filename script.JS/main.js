d94c0333465df6862d8bb144e98b96c5b0984cf6
Aydom <81184313+aydom669@users.noreply.github.com>
2022-01-30 15:05:23 -0300

    Desafio Landing Page

---
+++ script.JS/main.js
@@ -0,0 +1,9 @@
+
+
+const menu = () => {
+    var menu = document.querySelector(".nav");
+    var menuAmburg = document.querySelector(".btn__Amburge");
+
+    menu .classList.toggle("on");
+    menuAmburg.classList.toggle("me");
+}
